cbbf292ca3bda4532236ce2b4d2e618b4003fcf5
Эльвира Козлова <130339454+ksmsgfgnida@users.noreply.github.com>
2024-04-24 04:06:26 +0300

    Add files via upload

---
+++ main/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Performance(models.Model):
+    title = models.CharField(max_length=100)
+    age_limit = models.IntegerField(default=0)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='performances_photos/', default='performances_photos/default.jpg')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, to_field='name')  # Указываем поле 'name' для представления объекта категории
+
+    def __str__(self):
+        return f"{self.title} (ID категории: {self.category_id})"
+
+
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    avatar = models.ImageField(
+        verbose_name='Аватар',
+        upload_to='images/avatars/',
+        default='images/avatars/default.jpg',
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=('png', 'jpg', 'jpeg'))])
+
+
+    def get_absolute_url(self):
+        return reverse('profile', kwargs={'username': self.user.username})
---
+++ main/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for djangoProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+from .views import category_performance_list, profile
+from .views import UserRegisterView, UserLoginView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+    path('', views.index),
+    path('category/<str:category_id>/', category_performance_list, name='category_performance_list'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/<str:user>/', views.profile, name='profile'),
+
+]
